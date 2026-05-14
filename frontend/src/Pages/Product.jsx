fb2efb8045aadbe87385c0ef1a4e500487a1e80b
SyedAliHamzaRizvi <alihamza168039@gmail.com>
2026-05-15 01:57:06 +0500

    Fixed Vercel Routing

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../Context/ShopContext'
 import { assets } from '../assets/assets'
+import RelatedProducts from '../Components/RelatedProducts'
 
 
 const Product = () => {
@@ -97,6 +98,7 @@ useEffect(()=>{
      </div>
 
      {/* Display related products */}
+     <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
 
     </div>
   ) : <div className='opacity-0'></div>
