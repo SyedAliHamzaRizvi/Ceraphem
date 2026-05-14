98acaa7ef55b52fc645d1bb5e0daeda4c74db653
SyedAliHamzaRizvi <alihamza168039@gmail.com>
2026-05-15 02:14:52 +0500

    Related Products added to Collection Page

--- frontend/src/Components/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts.jsx
@@ -1,5 +1,7 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect , useState} from 'react'
 import { ShopContext } from '../Context/ShopContext'
+import Title from './Title'
+import ProductItem from './ProductItem'
 
 const RelatedProducts = ({category,subCategory}) => {
 
@@ -12,7 +14,7 @@ const RelatedProducts = ({category,subCategory}) => {
        productsCopy=productsCopy.filter((item)=>category===item.category)
        productsCopy=productsCopy.filter((item)=>subCategory===item.subCategory)
 
-       console.log(productsCopy.slice(0,5))
+       setRelated(productsCopy.slice(0,5))
       }
 
 
@@ -20,7 +22,17 @@ const RelatedProducts = ({category,subCategory}) => {
     },[products])
 
   return (
-    <div></div>
+    <div className='my-24'>
+      <div className='text-center text-3xl py-2'>
+        <Title text1={'RELATED'} text2={'PRODUCTS'}/>
+
+      </div>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-6'>
+        {related.map((item,index)=>(
+            <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
